d5bca7944cf84b5b94c88ddc6290c6434c758163
Warfvf <57152576+lnearlol@users.noreply.github.com>
2020-10-10 19:06:59 +0200

    Create token.c
    First token example

---
+++ token.c
@@ -0,0 +1,11 @@
+enum {IDENTIFICATOR, INT, FLOAT64, STRING, LITERAL_INT, LITERAL_FLOAT, LITERAL_STRING, OPERATOR, START_BLOCK, END_BLOCK, LEFT_BRACKET, RIGHT_BRACKET, EOL, EOF}
+typedef struct Token {
+    int t_typ;
+    char* id_name;
+    int* int_value;
+    char* string;
+    char[2] operator_name;
+    int *priority_operator;
+    struct Token *next;
+} token;
+
