bd90b1b2b35ac64f351f9cca62f0ad6ad101c7f4
Warfvf <57152576+lnearlol@users.noreply.github.com>
2020-10-25 16:41:48 +0100

    new_token

--- token.c
+++ token.c
@@ -1,6 +1,6 @@
 enum {IDENTIFICATOR, INT, FLOAT64, STRING, LITERAL_INT, LITERAL_FLOAT, LITERAL_STRING, OPERATOR, START_BLOCK, END_BLOCK, LEFT_BRACKET, RIGHT_BRACKET, EOL, EOF}
 typedef struct Token {
-    int t_typ;
+    int t_type;
     char* id_name;
     int* int_value;
     char* string;
