16138307617b5615cfdbce96f026c8adda89cf45
Viktoryia Tomason <62423376+Vikusik12@users.noreply.github.com>
2020-11-27 16:22:21 +0100

    Update generation.c

--- generation.c
+++ generation.c
@@ -28,7 +28,7 @@ void START_MAIN(){
 }
 
 void END_OF_MAIN(){
-	printf("POPFRAME\n");
+    printf("POPFRAME\n");
     printf("LABEL $endofmain\n");
 }
 
@@ -41,28 +41,25 @@ void GENERATION_INPUTI(){
     printf("LABEL $INPUTI\n");
     printf("PUSHFRAME\n");
     printf("DEFVAR LF@retval\n");
-    printf("READ LF@$retval int@\n");
+    printf("READ LF@$retval int\n");
     printf("DEFVAR LF@retval_check\n");
 
     printf("TYPE LF@$retval_check LF@retval\n");
-    printf("JUMPIFEQ $INPUTF_END LF@$retval_check  int@nil\n");
-    printf("PUSHS LF@retval\n");
+    printf("JUMPIFEQ $INPUTI_END LF@$retval_check nil@nil\n");
 
     printf("PUSHS LF@retval\n");
     printf("MOVE LF@retval$1 int@0\n");
     printf("PUSHS LF@retval$1\n");
     printf("JUMP $END\n");
 
-	printf("LABEL $INPUTI_END\n");
-    printf("MOVE LF@retval nil@nil\n");
+    printf("LABEL $INPUTI_END\n");
     printf("PUSHS LF@retval\n");
-
     printf("MOVE LF@retval$1 int@1\n");
     printf("PUSHS LF@retval$1\n");
 
     printf("LABEL $END\n");
-	printf("POPFRAME\n");
-	printf("RETURN\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 }
 void GENERATION_INPUTS(){
     printf("LABEL $INPUTS\n");
@@ -72,48 +69,45 @@ void GENERATION_INPUTS(){
     printf("DEFVAR LF@retval_check\n");
 
     printf("TYPE LF@$retval_check LF@retval\n");
-    printf("JUMPIFEQ $INPUTS_END LF@$retval_check  string@nil\n");
-    printf("PUSHS LF@retval\n");
+    printf("JUMPIFEQ $INPUTS_END LF@$retval_check  nil@nil\n");
 
     printf("PUSHS LF@retval\n");
     printf("MOVE LF@retval$1 int@0\n");
     printf("PUSHS LF@retval$1\n");
     printf("JUMP $END\n");
 
-	printf("LABEL $INPUTS_END\n");
-    printf("MOVE LF@retval nil@nil\n");
+    printf("LABEL $INPUTS_END\n");
     printf("PUSHS LF@retval\n");
-
     printf("MOVE LF@retval$1 int@1\n");
     printf("PUSHS LF@retval$1\n");
 
     printf("LABEL $END\n");
-	printf("POPFRAME\n");
-	printf("RETURN\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 }
 void GENERATION_INPUTF(){
     printf("LABEL $INPUTF\n");
     printf("PUSHFRAME\n");
     printf("DEFVAR LF@retval\n");
     printf("DEFVAR LF@retval_check\n");
-    printf("READ LF@$retval float@nil\n");
+    printf("READ LF@$retval float\n");
 
     printf("TYPE LF@$retval_check LF@retval\n");
-    printf("JUMPIFEQ $INPUTF_END LF@$retval_check  float@nil\n");
+    printf("JUMPIFEQ $INPUTF_END LF@$retval_check nil@nil\n");
+
     printf("PUSHS LF@retval\n");
     printf("MOVE LF@retval$1 int@0\n");
     printf("PUSHS LF@retval$1\n");
     printf("JUMP $END\n");
 
-	printf("LABEL $INPUTF_END\n");
-    printf("MOVE LF@retval nil@nil\n");
+    printf("LABEL $INPUTF_END\n");
     printf("PUSHS LF@retval\n");
     printf("MOVE LF@retval$1 int@1\n");
     printf("PUSHS LF@retval$1\n");
 
     printf("LABEL $END\n");
-	printf("POPFRAME\n");
-	printf("RETURN\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 }
 
 void GENERATION_LEN(){
@@ -121,7 +115,7 @@ void GENERATION_LEN(){
    printf("PUSHFRAME\n");
    printf("DEFVAR LF@retval\n");
    printf("DEFVAR LF@param1\n");
-   printf("MOVE LF@param1 LF@ 1\n");
+   printf("MOVE LF@param1 LF@s$0\n");
    printf("STRLEN LF@retval LF@param1\n");
    printf("POPFRAME\n");
    printf("RETURN\n");
@@ -160,14 +154,11 @@ void GENERATION_SUBSTR(){
     printf("DEFVAR LF@string\n");
     printf("DEFVAR LF@from\n");
     printf("DEFVAR LF@length_of_str\n");
-
     printf("DEFVAR LF@length\n");
 
-    printf("DEFVAR LF@length_help\n");
     printf("DEFVAR LF@length_helper\n");
     printf("DEFVAR LF@char\n");
     printf("DEFVAR LF@new_strlen\n");
-
 
     printf("MOVE LF@string LF@s$0\n");
     printf("MOVE LF@start LF@i$0\n");
@@ -176,30 +167,32 @@ void GENERATION_SUBSTR(){
     printf("MOVE LF@lenght_helper int@0\n");
 
     printf("STRLEN LF@length LF@strihg\n");//length = STRLEN(STRING)
+    printf("SUB LF@new_strlen LF@length int@1\n");//new_strlen = length - 1
 
     printf("DEFVAR LF@result\n");			
-    printf("LT LF@result LF@length_of_str int@0\n"); //n<O
-	printf("JUMPIFEQ $SUBSTR_END LF@result bool@true\n");
+    printf("LT LF@result LF@length_of_str int@0\n"); //n < O
+    printf("JUMPIFEQ $SUBSTR_END LF@result bool@true\n");
 
-    printf("LT LF@result LF@from int@0\n"); //i<O
-	printf("JUMPIFEQ $SUBSTR_END LF@result bool@true\n");
+    printf("EQ LF@result LF@length_of_str int@0\n"); //n == O
+    printf("JUMPIFEQ $SUBSTR_EMPTY LF@result bool@true\n");
 
-    printf("SUB LF@new_strlen LF@length int@1\n");//new_strlen = length - 1
-    printf("GT LF@result LF@from LF@new_strlen\n");//i>= length
+    printf("LT LF@result LF@from int@0\n"); //i < O
+    printf("JUMPIFEQ $SUBSTR_END LF@result bool@true\n");
+
+    printf("GT LF@result LF@from LF@new_strlen\n");//i >= length - 1
     printf("JUMPIFEQ $SUBSTR_END LF@result bool@true\n");
 
     printf("ADD LF@length_helper LF@from\n");
     printf("ADD LF@length_helper LF@length_of_str\n");//i + n
 
-    printf("GT LF@result LF@length_helper LF@new_strlen\n");
-	printf("JUMPIFEQ $SUBSTR_END LF@result bool@true\n");
-	printf("LABEL $SUBSTR_END LF@result bool@true\n");
-	printf("MOV LF@length_helper LF@new_strlen\n");
+    printf("GT LF@result LF@length_helper LF@length\n");//i + n > n ? n : i+n
+    printf("JUMPIFEQ $SUBSTR_LEN LF@result bool@true\n");
+    printf("JUMP $for_loop\n");
+    printf("LABEL $SUBSTR_LEN\n");
+    printf("MOV LF@length_helper LF@length\n");
 
-    printf("GETCHAR LF@char LF@string LF@from\n");
-
-	printf("LABEL $for_loop\n");
-    printf("JUMPIFEQ $SUBSTR_END LF@length_helper LF@from\n");
+    printf("LABEL $for_loop\n");
+    printf("JUMPIFEQ $SUBSTR_RET_0 LF@length_helper LF@from\n");
     printf("GETCHAR LF@char LF@string LF@from\n");
     printf("CONCAT LF@$retval LF@$retval LF@char\n");
     printf("ADD LF@from LF@from int@1\n");
@@ -211,16 +204,22 @@ void GENERATION_SUBSTR(){
     printf("PUSHS LF@retval$1\n");
     printf("JUMP $END\n");
 
-	printf("LABEL $SUBSTR_END\n");
+    printf("LABEL $SUBSTR_END\n");
     printf("MOVE LF@retval nil@nil\n");
     printf("PUSHS LF@retval\n");
-
     printf("MOVE LF@retval$1 int@1\n");
     printf("PUSHS LF@retval$1\n");
 
     printf("LABEL $END\n");
-	printf("POPFRAME\n");
-	printf("RETURN\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+
+    printf("LABEL $SUBSTR_EMPTY\n");
+    printf("MOVE LF@retval string@\000\n");
+    printf("PUSHS LF@retval\n");
+    printf("MOVE LF@retval$1 int@0\n");
+    printf("PUSHS LF@retval$1\n");
+    printf("JUMP $END\n");
 }
 
 void GENERATION_ORD(){
@@ -253,14 +252,13 @@ void GENERATION_ORD(){
     printf("JUMP $ORD_RET\n");
 
     printf("LABEL $ORD_END\n");
-    printf("MOVE LF@retval nil@nil\n");//?????
     printf("PUSHS LF@retval\n");
     printf("MOVE LF@retval$1 int@1\n");
     printf("PUSHS LF@retval$1\n");
 
     printf("LABEL $ORD_RET\n");
-	printf("POPFRAME\n");
-	printf("RETURN\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 }
 void GENERATION_CHR(){
     printf("LABEL $CHR\n");
@@ -273,36 +271,34 @@ void GENERATION_CHR(){
     printf("MOVE LF@int LF@i$0\n\n");
 
     printf("LT LF@right_int LF@int int@0\n");
-    printf("JUMPIFEQ $ORD_END LF@right_int bool@true\n");//i < 0
+    printf("JUMPIFEQ $CHR_END LF@right_int bool@true\n");//i < 0
 
     printf("GT LF@right_int LF@int int@255\n");
-    printf("JUMPIFEQ $ORD_END LF@right_int bool@true\n");//i > len(n)-1
+    printf("JUMPIFEQ $CHR_END LF@right_int bool@true\n");//i > len(n)-1
 
     printf("INT2CHAR LF@$retval LF@int\n");
 
     printf("PUSHS LF@retval\n");
     printf("MOVE LF@retval$1 int@0\n");
     printf("PUSHS LF@retval$1\n");
-    printf("JUMP $ORD_RET\n");
+    printf("JUMP $CHR_RET\n");
 
-    printf("LABEL $ORD_END\n");
-    printf("MOVE LF@retval nil@nil\n");
+    printf("LABEL $CHR_END\n");
     printf("PUSHS LF@retval\n");
     printf("MOVE LF@retval$1 int@1\n");
     printf("PUSHS LF@retval$1\n");
 
-    printf("LABEL $ORD_RET\n");
-	printf("POPFRAME\n");
-	printf("RETURN\n");
+    printf("LABEL $CHR_RET\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 }
-
 
 void print(){
     printf("LABEL $PRINT\n");
     printf("PUSHFRAME\n");
     printf("WRITE LF@ ");///???????
     printf("POPFRAME\n");
-	printf("RETURN\n");
+    printf("RETURN\n");
 }
 
 void GENERATION_PRINT(Token *token){
@@ -324,16 +320,12 @@ void GENERATION_PRINT(Token *token){
 }
 
 int main(){
-//FILE *program_code; was in the head of `token.h`
-//    program _code = fopen ("file.go", "r");
-
     token = malloc(sizeof(Token));
     token->next = NULL;
     Token *first = token;
 
     if(get_token(token)){
         fprintf(stderr, "\n     ERROR!!\n\n");
-        // return 1;
     }
 
 
@@ -342,22 +334,11 @@ int main(){
         if(token->type == TOKEN_TYPE_EOFILE)
             break;
         if(!strcmp(token->data, "inputs"))
-    GENERATION_INPUTS();
-        // token->next = malloc (sizeof(Token));
-        // token = token->next;
-        // token->next = NULL;
-        // get_token(token);
+            GENERATION_INPUTS();
         if(set_next_token()){
             fprintf(stderr, "\n     ERROR!!\n\n");
-            // return 1;
         }
     }
-    
-    // while(first != NULL){
-    //     printf("prev-token: %s\n", first->data);
-    //     printf("TOKEN TYPE: %d\n", first->type);
-    //     first = first->next;
-    // }
 
     dtor(first);
     //fclose(program _code);
