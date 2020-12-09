0770da4f02beecb77321a7823a159de25d33f45e
Vladislav <vlad1023333@gmail.com>
2020-12-09 23:36:33 +0400

    Comments fixes

--- expression_translator.h
+++ expression_translator.h
@@ -1,5 +1,18 @@
+/**
+ * @file expression_translator.c
+ *
+ * @brief Header file for expression_translator.c for processing expressions
+ *
+ * IFJ Projekt 2020, Tým 55
+ *
+ * @author <xpastu04> Pastushenko Vladislav
+ */
+
 #include "parser.h"
 
+/**
+ * Constants for working with stack of tokens
+ */
 #define INIT_SIZE 10
 #define MULTIPLIER 1.5
 #define STACK_OVERFLOW  -100
